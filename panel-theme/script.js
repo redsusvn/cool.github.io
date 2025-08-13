b1740608210872f4f061d23acddd6764f66fd134
hostsieure <hostsieure.services@gmail.com>
2025-08-13 15:10:25 +0700

    Update script.js

--- panel-theme/script.js
+++ panel-theme/script.js
@@ -1,4 +1,11 @@
-const searchDiv = document.querySelector('form[action="https://ifastnet.com/portal/domainchecker.php"]').parentNode;
-if (searchDiv && searchDiv.parentNode) {
-  searchDiv.parentNode.remove();
+const searchForm = document.querySelector('form[action="https://ifastnet.com/portal/domainchecker.php"]');
+if (searchForm) {
+  const searchDiv = searchForm.closest('.panel.panel-widget'); // Get the closest parent with panel and panel-widget classes
+  if (searchDiv && searchDiv.parentNode) {
+    searchDiv.parentNode.remove();
+  }
+}
+const sidebar = document.getElementById('sidebar');
+if (sidebar) {
+  sidebar.remove();
 }
