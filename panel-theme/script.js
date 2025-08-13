478a4b9bd8f9a390c73443d520cc2ef55575b29a
hostsieure <hostsieure.services@gmail.com>
2025-08-13 15:24:45 +0700

    Update script.js

--- panel-theme/script.js
+++ panel-theme/script.js
@@ -1,11 +1,13 @@
-const searchForm = document.querySelector('form[action="https://ifastnet.com/portal/domainchecker.php"]');
-if (searchForm) {
-  const searchDiv = searchForm.closest('.panel.panel-widget'); // Get the closest parent with panel and panel-widget classes
-  if (searchDiv && searchDiv.parentNode) {
-    searchDiv.parentNode.remove();
+setTimeout(function() {
+  const searchForm = document.querySelector('form[action="https://ifastnet.com/portal/domainchecker.php"]');
+  if (searchForm) {
+    const searchDiv = searchForm.closest('.panel.panel-widget'); 
+    if (searchDiv && searchDiv.parentNode) {
+      searchDiv.parentNode.remove();
+    }
   }
-}
-const sidebar = document.getElementById('sidebar');
-if (sidebar) {
-  sidebar.remove();
-}
+  const sidebar = document.getElementById('sidebar');
+  if (sidebar) {
+    sidebar.remove();
+  }
+}, 3500);
